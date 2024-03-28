64dea430cfa514ef9fc77c9e01a739c3c0214ef0
lmangall <l.mangallon@gmail.com>
2024-03-29 00:46:10 +0100

    hello.cgi and documentation first version

---
+++ docs/cgi-bin/hello.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib> // For getenv() and system()
+
+using namespace std;
+
+int main() {
+    // Set the HTTP header
+    cout << "Content-type: text/html\n\n";
+
+    // Start of the HTML content
+    cout << "<html>\n";
+    cout << "<head>\n";
+    cout << "<title>Simple C++ CGI Script</title>\n";
+    cout << "</head>\n";
+    cout << "<body>\n";
+    cout << "<h1>Hello from C++ CGI!</h1>\n";
+
+    // Getting the QUERY_STRING environment variable
+    char* query_string = getenv("QUERY_STRING");
+    if (query_string) {
+        cout << "<p>Query String: " << query_string << "</p>\n";
+    } else {
+        cout << "<p>No query string provided.</p>\n";
+    }
+
+    // End of the HTML content
+    cout << "</body>\n";
+    cout << "</html>\n";
+
+    return 0;
+}
+
+
+//    $> cpp hello.cpp -o hello.cgi
+//    $> chmod +x hello.cgi
