9886e85219e03ab32c68f814f70c6fa728166c54
552020 <stefanolombardo@posteo.de>
2024-04-06 18:31:42 +0200

    WIP

--- HTTPRequest.hpp
+++ HTTPRequest.hpp
@@ -2,10 +2,9 @@
 #include <iostream>
 #include <unistd.h>
 
-// It is RECOMMENDED that all HTTP 
+// It is RECOMMENDED that all HTTP
 // senders and recipients support, at a minimum, request-line lengths of 8000 octets
-# define MAX_URI 200
-
+#define MAX_URI 200
 
 // #include <string.h>
 // #include <stdio.h>
@@ -14,19 +13,21 @@
 // std::cout << obj.getStatusCode() << std::endl;
 // insert after read in the main to test
 
-class HTTPRequest{
-	public:
-		HTTPRequest();
-		HTTPRequest(char *request);
-		~HTTPRequest();
-		std::string getMethod() const;
-		int			getStatusCode() const;
-		std::string getRequestTarget() const;
-		std::string getProtocolVersion() const;
-	private:
-		int			parseRequestLine(char *request);
-		int			_statusCode;
-		std::string _method;
-		std::string	_requestTarget;
-		std::string _protocolVersion;
+class HTTPRequest
+{
+  public:
+	HTTPRequest();
+	HTTPRequest(const char *request);
+	~HTTPRequest();
+	std::string getMethod() const;
+	int getStatusCode() const;
+	std::string getRequestTarget() const;
+	std::string getProtocolVersion() const;
+
+  private:
+	int parseRequestLine(char *request);
+	int _statusCode;
+	std::string _method;
+	std::string _requestTarget;
+	std::string _protocolVersion;
 };
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <poll.h>
+#include <vector>
 #include "include/webserv.hpp"
 
 const int PORT = 8080;
@@ -57,27 +58,87 @@ int main()
 		perror("In listen");
 		exit(EXIT_FAILURE);
 	}
+	std::vector<struct pollfd> fds;
 	// Create and initailize the pollfd structure for the server socket
-	struct pollfd fds[1];
-	fds[0].fd = server_fd;
+	// struct pollfd fds[1];
+	// fds[0].fd = server_fd;
+	// fds[0].events = POLLIN;
+	struct pollfd server_fd_poll;
+	server_fd_poll.fd = server_fd;
 	// We are listening for incoming connections, i.e. the socket will be readable
-	fds[0].events = POLLIN;
+	server_fd_poll.events = POLLIN;
+	server_fd_poll.revents = 0;
+	fds.push_back(server_fd_poll);
+	// Set server socket to non-blocking
+	// Not necessary since we are using poll
+	// int flags = fcntl(server_fd, F_GETFL, 0);
+	// fcntl(server_fd, F_SETFL, flags | O_NONBLOCK);
+
 	while (1)
 	{
 		std::cout << "++++++++++++++ Waiting for new connection +++++++++++++++" << std::endl;
-		int ret = poll(fds, 1, -1); // -1 means wait indefinitely
+		// int ret = poll(fds, 1, -1); // -1 means wait indefinitely
+		// fds.data() returns a pointer to the underlying array of pollfd structures
+		int ret = poll(fds.data(), fds.size(), -1); // -1 means wait indefinitely
+		// if the server socket is readable, then a new connection is available
 		if (ret > 0)
 		{
-			if (fds[0].revents & POLLIN)
+			// We loop over all the file descriptors in the pollfd structure
+			for (size_t i = 0; i < fds.size(); i++)
 			{
-				std::cout << "New connection detected" << std::endl;
-				int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-				if (new_socket < 0)
+				if (fds[i].revents & POLLIN)
 				{
-					perror("In accept");
-					exit(EXIT_FAILURE);
+
+					// Check if the server socket is readable: fds[0] is the server socket
+					if (i == 0)
+					{
+						std::cout << "New connection detected" << std::endl;
+						// accept is blocking but we know that the server socket is readable
+						int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+						if (new_socket >= 0)
+						{
+							// Add the new socket to the pollfd structure
+							struct pollfd new_socket_poll;
+							new_socket_poll.fd = new_socket;
+							new_socket_poll.events = POLLIN;
+							new_socket_poll.revents = 0;
+							fds.push_back(new_socket_poll);
+						}
+						else
+						{
+							perror("In accept");
+							// TODO: Decide how to handle accept failure: exit, continue, or handle gracefully
+						}
+					}
+					else
+					{
+						// Handle the client socket
+						handleConnection(fds[i].fd);
+						// Remove the client socket from the pollfd structure
+						fds.erase(fds.begin() + i);
+					}
 				}
-				handleConnection(new_socket);
+				else if (fds[i].revents & (POLLERR | POLLHUP | POLLNVAL))
+				{
+					if (i == 0)
+					{
+						// Handle the server socket error
+						// Log the error if necessary
+						// Attempt recovery or initiate a graceful shutdown
+						// Possibly alert administrators
+						perror("poll server socket error");
+						// exit(EXIT_FAILURE);
+					}
+					else
+					{
+						// Handle the client socket error
+						close(fds[i].fd);
+						fds.erase(fds.begin() + i);
+						perror("poll client socket error");
+						--i; // Adjust the index to account for the removed element
+							 // exit(EXIT_FAILURE);
+					}
+				}
 			}
 		}
 		else if (ret == 0)
@@ -86,9 +147,24 @@ int main()
 		}
 		else
 		{
-			perror("poll");
-			exit(EXIT_FAILURE);
+			// handle the errors on poll on the server socket
+			if (errno == EINTR)
+			{
+				// poll was interrupted by a signal
+				// Log the interruption if necessary
+				continue; // Retry the poll operation
+			}
+			else
+			{
+				// poll failes: EBADF, EFAULT, EINVAL, ENOMEM
+				// Log critical error details
+				// Attempt recovery or initiate a graceful shutdown
+				// Possibly alert administrators
+				perror("poll");
+				// exit(EXIT_FAILURE);
+			}
 		}
 	}
+
 	return 0;
 }
