6ecaad040a13a1b61e147487fb6fa3e2768950b4
lmangall <l.mangallon@gmail.com>
2024-04-06 16:51:37 +0200

    better explanations on the metadata-vars

--- src/CGI_handler.cpp
+++ src/CGI_handler.cpp
@@ -76,7 +76,7 @@ void HTTPRequestToMetaVars(char* rawRequest, Environment& env) {
     // The name and version of the HTTP server (Format: name/version)
     env.setVar("SERVER_SOFTWARE", "Server_of_people_identifying_as_objects/1.0");
     // The host name, DNS alias, or IP address of the server
-    env.setVar("SERVER_NAME", "The_Objects.com");
+    env.setVar("SERVER_NAME", "The_objects.com");
     // The CGI specification revision the server is using (Format: CGI/version)
     env.setVar("GATEWAY_INTERFACE", "CGI/1.1");
 
@@ -84,25 +84,31 @@ void HTTPRequestToMetaVars(char* rawRequest, Environment& env) {
     // The name and revision of the protocol the request was made in (Format: protocol/revision)
     env.setVar("SERVER_PROTOCOL", request.getProtocolVersion());
     // The port number on which the request was received
-    env.setVar("SERVER_PORT", "8080"); //     ---> how do I set it programmatically ?
+    env.setVar("SERVER_PORT", "8080"); //     ---> how to set it programmatically ? from the macro ?
     // Additional path information from the client's request URL
     env.setVar("PATH_INFO", ""); 
     // The translated physical path the request refers to (after virtual to physical conversion by the server)
     env.setVar("PATH_TRANSLATED", ""); // Needs specific server-side logic to determine
     // The virtual path to the script being executed
-    env.setVar("SCRIPT_NAME", ""); // Needs to be set based on the script's location
+    env.setVar("SCRIPT_NAME", "");
     // The query string from the URL sent by the client
-    env.setVar("QUERY_STRING", ""); // Needs parsing from the request
-    // The host name of the client making the request
+    env.setVar("QUERY_STRING", "request.getQueryString()");
+
+    //The REMOTE_HOST variable contains the fully qualified domain name of
+   //the client sending the request to the server
     env.setVar("REMOTE_HOST", ""); // Might require reverse DNS lookup
-    // The IP address of the client
+    // network address (IP) of the client sending the request to the server.
     env.setVar("REMOTE_ADDR", ""); // Needs to be obtained from the request/connection
+
+    //AUTHENTICATION :
     // The authentication method used to protect the script
     env.setVar("AUTH_TYPE", ""); // Depends on server configuration
     // The client's username, if the script is protected and the server supports authentication
     env.setVar("REMOTE_USER", ""); // Depends on server and authentication method
     // The remote (client's) username from RFC 931 identification; for log purposes only
     env.setVar("REMOTE_IDENT", ""); // Requires specific server support
+
+    //ONLY FOR POST REQUESTS ?    OR NOT ?
     // The content type attached to the request, if any
     env.setVar("CONTENT_TYPE", ""); // Needs to be parsed from the request headers
     // The length of the content sent by the client
