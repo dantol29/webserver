2932b5dc6be7c607637e53afefc0642c7f3cb1bd
552020 <stefanolombardo@posteo.de>
2024-04-09 08:58:07 +0200

    It does compile after merge

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -1,128 +1,146 @@
 #include "HTTPRequest.hpp"
 #include <string.h>
 
-bool		isOrigForm(std::string& requestTarget, int &queryStart);
-bool		fileExists(std::string& requestTarget, bool isOriginForm, int queryStart);
-void		skipRequestLine(const char *request, unsigned int& i);
-void		skipHeader(const char *request, unsigned int& i);
-bool		hasMandatoryHeaders(HTTPRequest& obj);
-bool		hasCRLF(const char* request, unsigned int& i, int mode);
-std::string		extractValue(std::string& variables, int &i);
-std::string 	extractKey(std::string& variables, int &i, int startPos);
-std::string 	extractRequestTarget(const char *request, unsigned int& i);
-std::string		extractVariables(std::string& requestTarget, bool& isOriginForm);
-std::string 	extractProtocolVersion(const char *request, unsigned int& i);
-std::string		extractMethod(const char *request, unsigned int& i);
-std::string		extractHeaderKey(const char *request, unsigned int& i);
-std::string		extractHeaderValue(const char *request, unsigned int& i);
-unsigned int	extractLineLength(const char *request, unsigned int& i);
-std::string		extractLine(const char *request, unsigned int& i, const unsigned int& size);
-int			parseBody();
+bool isOrigForm(std::string &requestTarget, int &queryStart);
+bool fileExists(std::string &requestTarget, bool isOriginForm, int queryStart);
+void skipRequestLine(const char *request, unsigned int &i);
+void skipHeader(const char *request, unsigned int &i);
+bool hasMandatoryHeaders(HTTPRequest &obj);
+bool hasCRLF(const char *request, unsigned int &i, int mode);
+std::string extractValue(std::string &variables, int &i);
+std::string extractKey(std::string &variables, int &i, int startPos);
+std::string extractRequestTarget(const char *request, unsigned int &i);
+std::string extractVariables(std::string &requestTarget, bool &isOriginForm);
+std::string extractProtocolVersion(const char *request, unsigned int &i);
+std::string extractMethod(const char *request, unsigned int &i);
+std::string extractHeaderKey(const char *request, unsigned int &i);
+std::string extractHeaderValue(const char *request, unsigned int &i);
+unsigned int extractLineLength(const char *request, unsigned int &i);
+std::string extractLine(const char *request, unsigned int &i, const unsigned int &size);
+int parseBody();
 
-HTTPRequest::HTTPRequest() : _statusCode(200), _isChunked(false), _method(""), \
-_requestTarget(""), _protocolVersion(""){
-
+HTTPRequest::HTTPRequest() : _statusCode(200), _isChunked(false), _method(""), _requestTarget(""), _protocolVersion("")
+{
 }
 
-HTTPRequest::HTTPRequest(const HTTPRequest& obj){
+HTTPRequest::HTTPRequest(const HTTPRequest &obj)
+{
 	_statusCode = obj._statusCode;
 	_isChunked = obj._isChunked;
 	_method = obj._method;
 	_requestTarget = obj._requestTarget;
 	_protocolVersion = obj._protocolVersion;
 	_body = obj._body;
-	_headers  = obj._headers;
+	_headers = obj._headers;
 	_queryString = obj._queryString;
 }
 
-HTTPRequest& HTTPRequest::operator=(const HTTPRequest& obj){
-	if (this != &obj){
+HTTPRequest &HTTPRequest::operator=(const HTTPRequest &obj)
+{
+	if (this != &obj)
+	{
 		_statusCode = obj._statusCode;
 		_isChunked = obj._isChunked;
 		_method = obj._method;
 		_requestTarget = obj._requestTarget;
 		_protocolVersion = obj._protocolVersion;
 		_body = obj._body;
-		_headers  = obj._headers;
+		_headers = obj._headers;
 		_queryString = obj._queryString;
 	}
 	return (*this);
 }
 
-HTTPRequest::~HTTPRequest(){
-
+HTTPRequest::~HTTPRequest()
+{
 }
 
-HTTPRequest::HTTPRequest(char *request){
+HTTPRequest::HTTPRequest(const char *request)
+{
 	if (strlen(request) < 10)
 		_statusCode = 400;
-	else{
+	else
+	{
 		_statusCode = parseRequestLine(request);
 		if (_statusCode == 200)
 			_statusCode = parseHeaders(request);
 		else if (_statusCode == 200 && !_isChunked)
-			parseBody();// parse regular body
+			parseBody(); // parse regular body
 	}
 }
 
-std::string HTTPRequest::getMethod() const{
+std::string HTTPRequest::getMethod() const
+{
 	return (_method);
 }
 
-std::string HTTPRequest::getProtocolVersion() const{
+std::string HTTPRequest::getProtocolVersion() const
+{
 	return (_protocolVersion);
 }
 
-std::string HTTPRequest::getRequestTarget() const{
+std::string HTTPRequest::getRequestTarget() const
+{
 	return (_requestTarget);
 }
 
-int	HTTPRequest::getStatusCode() const{
+int HTTPRequest::getStatusCode() const
+{
 	return (_statusCode);
 }
 
-bool	HTTPRequest::getIsChunked() const{
+bool HTTPRequest::getIsChunked() const
+{
 	return (_isChunked);
 }
 
-std::multimap<std::string, std::string>	HTTPRequest::getQueryString() const{
+std::multimap<std::string, std::string> HTTPRequest::getQueryString() const
+{
 	return (_queryString);
 }
 
-std::multimap<std::string, std::string>	HTTPRequest::getHeaders() const{
+std::multimap<std::string, std::string> HTTPRequest::getHeaders() const
+{
 	return (_headers);
 }
 
-std::pair<std::string, std::string> HTTPRequest::getHeaders(std::string key) const{
+std::pair<std::string, std::string> HTTPRequest::getHeaders(std::string key) const
+{
 	std::multimap<std::string, std::string>::const_iterator it;
 
-	for (it = _headers.begin(); it != _headers.end(); ++it){
+	for (it = _headers.begin(); it != _headers.end(); ++it)
+	{
 		if (it->first == key)
 			return (std::make_pair(it->first, it->second));
 	}
 	return (std::make_pair("", ""));
 }
 
-bool HTTPRequest::getIsChunkFinish() const{
+bool HTTPRequest::getIsChunkFinish() const
+{
 	return (_isChunkFinish);
 }
 
-std::vector<std::string> HTTPRequest::getBody() const{
+std::vector<std::string> HTTPRequest::getBody() const
+{
 	return (_body);
 }
 
-void	HTTPRequest::setIsChunked(bool n){
+void HTTPRequest::setIsChunked(bool n)
+{
 	_isChunked = n;
 }
 
-bool	HTTPRequest::saveVariables(std::string& variables)
+bool HTTPRequest::saveVariables(std::string &variables)
 {
-	int	startPos = 0;
-	std::string	key;
-	std::string	value;
+	int startPos = 0;
+	std::string key;
+	std::string value;
 
-	for (int i = 0; i < (int)variables.length(); i++){
-		if (variables[i] == '='){
+	for (int i = 0; i < (int)variables.length(); i++)
+	{
+		if (variables[i] == '=')
+		{
 			key = extractKey(variables, i, startPos);
 			if (key.empty())
 				return (false);
@@ -136,21 +154,21 @@ bool	HTTPRequest::saveVariables(std::string& variables)
 	return (true);
 }
 
-int	HTTPRequest::parseRequestLine(const char *request)
+int HTTPRequest::parseRequestLine(const char *request)
 {
-	unsigned int	i = 0;
-	bool			isOriginForm = false;
-	std::string		variables;
+	unsigned int i = 0;
+	bool isOriginForm = false;
+	std::string variables;
 
 	_method = extractMethod(request, i);
-	if (_method.empty()) // A server that receives a method longer than any that it implements
-		return (501); // SHOULD respond with a 501 (Not Implemented).
+	if (_method.empty())	 // A server that receives a method longer than any that it implements
+		return (501);		 // SHOULD respond with a 501 (Not Implemented).
 	if (request[i++] != ' ') // single space
 		return (400);
 	_requestTarget = extractRequestTarget(request, i);
-	if (_requestTarget.empty()) // A server that receives a request-target longer 
-		return (414); // than any URI it wishes to parse MUST respond with a 414 (URI Too Long).
-	if (request[i++] != ' ') // single space
+	if (_requestTarget.empty()) // A server that receives a request-target longer
+		return (414);			// than any URI it wishes to parse MUST respond with a 414 (URI Too Long).
+	if (request[i++] != ' ')	// single space
 		return (400);
 	variables = extractVariables(_requestTarget, isOriginForm);
 	if (variables.empty())
@@ -163,22 +181,24 @@ int	HTTPRequest::parseRequestLine(const char *request)
 		return (400);
 	if (!hasCRLF(request, i, 0))
 		return (400);
-	return (200); 
+	return (200);
 }
 
-int	HTTPRequest::parseChunkedBody(const char *request)
+int HTTPRequest::parseChunkedBody(const char *request)
 {
-	unsigned int	i = 0;
-	int				size = 0;
-	std::string		line;
+	unsigned int i = 0;
+	int size = 0;
+	std::string line;
 
 	skipHeader(request, i);
-	while (request[i]){
+	while (request[i])
+	{
 		size = extractLineLength(request, i);
-		if (size <= 0){
+		if (size <= 0)
+		{
 			if (size == -1)
 				return (400);
-			break ;
+			break;
 		}
 		line = extractLine(request, i, size);
 		if (line.empty())
@@ -194,13 +214,14 @@ int	HTTPRequest::parseChunkedBody(const char *request)
 
 int HTTPRequest::parseHeaders(const char *request)
 {
-	unsigned int	i;
-	std::string		key;
-	std::string		value;
+	unsigned int i;
+	std::string key;
+	std::string value;
 
 	i = 0;
 	skipRequestLine(request, i);
-	while (request[i]){
+	while (request[i])
+	{
 		key = extractHeaderKey(request, i);
 		if (key.empty())
 			return (400);
@@ -213,15 +234,15 @@ int HTTPRequest::parseHeaders(const char *request)
 		if (request[i] != '\r' || request[i + 1] != '\n')
 			return (400);
 		_headers.insert(std::make_pair(key, value));
-		i += 2; // skip '\r' and '\n'
+		i += 2;											  // skip '\r' and '\n'
 		if (request[i] == '\r' && request[i + 1] == '\n') // end of header section
-			break ;
+			break;
 	}
 	if (request[i] != '\r' || request[i + 1] != '\n') // end of header section
 		return (400);
 	if (!hasMandatoryHeaders(*this))
 		return (400);
-	if (_method == "GET" && request[i + 2]) //has something after headers
+	if (_method == "GET" && request[i + 2]) // has something after headers
 		return (400);
 	return (200);
 }
--- src/HTTPRequest.hpp
+++ src/HTTPRequest.hpp
@@ -11,41 +11,48 @@
 // senders and recipients support, at a minimum, request-line lengths of 8000 octets
 #define MAX_URI 200
 
-// #include <string.h>
-// #include <stdio.h>
-// #include "HTTPRequest.hpp"
-// HTTPRequest obj(buffer);
-// std::cout << obj.getStatusCode() << std::endl;
-// insert after read in the main to test
-
 class HTTPRequest
 {
   public:
-	HTTPRequest();
 	HTTPRequest(const HTTPRequest &obj);
 	HTTPRequest &operator=(const HTTPRequest &obj);
 	~HTTPRequest();
-	HTTPRequest(char *request);
+	HTTPRequest(const char *request);
 	std::string getMethod() const;
 	int getStatusCode() const;
 	std::string getRequestTarget() const;
 	std::string getProtocolVersion() const;
-	std::map<std::string, std::string> getStorage() const;
-	bool addStorage(std::string key, std::string value);
+	bool getIsChunked() const;
+	bool getIsChunkFinish() const;
+	std::multimap<std::string, std::string> getQueryString() const;
+	std::multimap<std::string, std::string> getHeaders() const;
+	std::pair<std::string, std::string> getHeaders(std::string key) const;
+	std::vector<std::string> getBody() const;
+	void setIsChunked(bool a);
+	int parseChunkedBody(const char *request);
 
   private:
-	int parseRequestLine(char *request);
+	HTTPRequest();
+	int parseRequestLine(const char *request);
+	int parseHeaders(const char *request);
+	bool saveVariables(std::string &variables);
 	int _statusCode;
-	std::map<std::string, std::string> _storage;
+	bool _isChunked;
+	bool _isChunkFinish;
 	std::string _method;
 	std::string _requestTarget;
 	std::string _protocolVersion;
+	std::multimap<std::string, std::string> _queryString;
+	std::multimap<std::string, std::string> _headers;
+	std::vector<std::string> _body;
 };
 
-bool isOrigForm(std::string &requestTarget, int &queryStart);
-bool fileExists(std::string &requestTarget, bool isOriginForm, int queryStart);
-std::string extractValue(std::string &variables, int &i);
-std::string extractKey(std::string &variables, int &i, int startPos);
-std::string extractRequestTarget(char *request, int &i);
-std::string extractProtocolVersion(char *request, int &i);
-std::string extractMethod(char *request, int &i);
+// bool isOrigForm(std::string &requestTarget, int &queryStart);
+// bool fileExists(std::string &requestTarget, bool isOriginForm, int queryStart);
+// std::string extractValue(std::string &variables, int &i);
+// std::string extractKey(std::string &variables, int &i, int startPos);
+// std::string extractRequestTarget(char *request, int &i);
+// std::string extractProtocolVersion(char *request, int &i);
+// std::string extractMethod(char *request, int &i);
+
+#endif // HTTPREQUEST_HPP
