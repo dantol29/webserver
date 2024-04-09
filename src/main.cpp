9dd95d3990dde04c75ba4e5bf43f7ddbf85815d9
552020 <stefanolombardo@posteo.de>
2024-04-09 12:37:29 +0200

    clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-#include <cstdlib> // For exit() and EXIT_FAILURE
-#include <iostream>
-#include <netinet/in.h> // For sockaddr_in
-#include <sys/socket.h> // For socket functions
-#include <unistd.h>		// For read, write, and close
-#include <fstream>
-#include <sstream>
-#include <poll.h>
-#include <vector>
-#include "webserv.hpp"
 #include "Server.hpp"
 
 int main()
