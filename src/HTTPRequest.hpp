08efec4cbfebb76c4c142ae43f6566383eaa14c2
552020 <stefanolombardo@posteo.de>
2024-04-19 13:08:19 +0200

    remove utils function from http request object

--- src/HTTPRequest.hpp
+++ src/HTTPRequest.hpp
@@ -4,9 +4,6 @@
 #include <string>
 #include <map>
 
-// TODO: remove this later
-#define MAX_URI 200
-
 class HTTPRequest
 {
   public:
@@ -34,26 +31,6 @@ class HTTPRequest
 	std::multimap<std::string, std::string> _queryString;
 	std::multimap<std::string, std::string> _headers;
 	std::vector<std::string> _body;
-
-	// UTILS
-	bool saveVariables(std::string &variables);
-	void makeHeadersLowCase();
-	bool isValidHost(std::string host);
-	bool isValidContentType(std::string type);
-	bool isOrigForm(std::string &requestTarget, int &queryStart);
-	void skipRequestLine(const char *request, unsigned int &i);
-	void skipHeader(const char *request, unsigned int &i);
-	bool hasMandatoryHeaders(HTTPRequest &obj);
-	std::string extractValue(std::string &variables, int &i);
-	std::string extractKey(std::string &variables, int &i, int startPos);
-	std::string extractRequestTarget(const char *request, unsigned int &i);
-	std::string extractVariables(std::string &requestTarget, bool &isOriginForm);
-	std::string extractProtocolVersion(const char *request, unsigned int &i);
-	std::string extractMethod(const char *request, unsigned int &i);
-	std::string extractHeaderKey(const char *request, unsigned int &i);
-	std::string extractHeaderValue(const char *request, unsigned int &i);
-	unsigned int extractLineLength(const char *request, unsigned int &i);
-	std::string extractLine(const char *request, unsigned int &i, const unsigned int &size);
 };
 
 std::ostream &operator<<(std::ostream &out, const HTTPRequest &a);
