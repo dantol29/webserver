b7bb2ab3604ce8711027cdd08895144f18900e98
552020 <stefanolombardo@posteo.de>
2024-05-01 11:23:09 +0200

    restore the logic that probably Daniil broke

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,21 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	if (argc != 2)
+	if (argc > 2)
+	{
+		std::cout << "Usage: ./webserv [<config_file>]" << std::endl;
 		return (1);
-	ConfigFile a(argv[1]);
-	if (!a.getErrorMessage().empty()){
-		std::cout << a.getErrorMessage() << std::endl;
-		return 0;
 	}
-	std::cout << a << std::endl;
+	else if (argc == 2)
+	{
+		ConfigFile a(argv[1]);
+		if (!a.getErrorMessage().empty())
+		{
+			std::cout << a.getErrorMessage() << std::endl;
+			return 0;
+		}
+		std::cout << a << std::endl;
+	}
 	Server webserv;
 	webserv.startListening();
 	webserv.startPollEventLoop();
