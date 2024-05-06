056ec1ba72c97f9b94e50897f69dbf0ebefa4b93
552020 <stefanolombardo@posteo.de>
2024-05-06 10:52:45 +0200

    complete copy constructor and copy assignment operator with the missing properties and add copy constructor and copy assignment operator for HTTPRequest

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -18,6 +18,8 @@ Connection::Connection(const Connection &other)
 {
 	_pollFd = other._pollFd;
 	_response = other._response;
+	_request = other._request;
+	_parser = other._parser;
 	_hasReadSocket = other._hasReadSocket;
 	_hasFinishedReading = other._hasFinishedReading;
 	_hasDataToSend = other._hasDataToSend;
@@ -33,6 +35,13 @@ Connection &Connection::operator=(const Connection &other)
 	{
 		_pollFd = other._pollFd;
 		_response = other._response;
+		_request = other._request;
+		_parser = other._parser;
+		_hasReadSocket = other._hasReadSocket;
+		_hasFinishedReading = other._hasFinishedReading;
+		_hasDataToSend = other._hasDataToSend;
+		_hasFinishedSending = other._hasFinishedSending;
+		_canBeClosed = other._canBeClosed;
 	}
 	std::cout << "Connection object assigned" << std::endl;
 	return *this;
